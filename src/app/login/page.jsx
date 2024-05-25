61ae95b01b5fefb1f02397dff203095460117e43
unknown <dammyvicky1245@gmail.com>
2024-05-25 14:55:07 +0100

    login error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,9 +5,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { CgDanger } from "react-icons/cg";
 
-
 const Login = () => {
-    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
@@ -25,59 +23,54 @@ const Login = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                console.log(data)
-                const token = data.payload.token
-                const fullName = data.payload.user.fullName
-                const userRole = data.payload.user.role
-                // console.log('token received', token)
-                localStorage.setItem("authToken", token)
-                localStorage.setItem("fullName", fullName)
-                // console.log(token)
+                console.log('API response data:', data); // Log the response data
 
-               
+                if (data && data.payload && data.payload.user) {
+                    const token = data.payload.token;
+                    const fullName = data.payload.user.fullName;
+                    const userRole = data.payload.user.role;
 
-                // Handle successful login
-                if (userRole === 'PATIENT') {
-                    // Redirect to patient dashboard
-                    window.location.href = '/patient-dashboard';
-                } else if (userRole === 'ADMIN') {
-                    // Redirect to admin dashboard
-                    window.location.href = '/admin-dashboard';
-                } else if (userRole === 'CLINICIAN') {
-                    // Redirect to clinician dashboard
-                    window.location.href = '/clinician-dashboard';
-                } else {
+                    localStorage.setItem("authToken", token);
+                    localStorage.setItem("fullName", fullName);
 
-                           // Handle other cases or provide a default redirection
-                    window.location.href = '/';
-                        }
-             } else {
-                const errorMessage = await response.text();
-                const beginningSlice = errorMessage.slice(12, -1); 
-                const endSlice = beginningSlice.slice(0, -2)
-                setMessage(endSlice);
+                    // Handle successful login
+                    switch (userRole) {
+                        case 'PATIENT':
+                            window.location.href = '/patient-dashboard';
+                            break;
+                        case 'ADMIN':
+                            window.location.href = '/admin-dashboard';
+                            break;
+                        case 'CLINICIAN':
+                            window.location.href = '/clinician-dashboard';
+                            break;
+                        default:
+                            window.location.href = '/';
+                            break;
                     }
-                } catch (error) {
-                    console.error('Error', error);
-                    alert(error);
+                } else {
+                    setMessage("Invalid response structure from the server.");
+                    console.error('Invalid response structure:', data); // Log the invalid structure
                 }
-            };
-    
-    
-    return (
-    
+            } else {
+                const errorMessage = await response.text();
+                setMessage(errorMessage.slice(12, -2));
+            }
+        } catch (error) {
+            console.error('Error', error);
+            setMessage('An unexpected error occurred. Please try again.');
+        }
+    };
 
+    return (
         <div className={styles.loginContainer}>
             <div className={styles.imgContainer}>
-                <p className={styles.properDiagnosis}>
-                    Proper Diagnosis
-                </p>
+                <p className={styles.properDiagnosis}>Proper Diagnosis</p>
                 <div className={styles.physicianContainer}>
                     <Image src="/physician.png" alt="physician" width={900} height={500} />
                 </div>
-
                 <div className={styles.logoContainer}>
-                    <Image src="/logo.png" alt="logo" width={53.99} height={53.99} />
+                    <Image src="/logo.png" alt="logo" width={54} height={54} />
                     <p className={styles.logo}>CDSS</p>
                 </div>
             </div>
@@ -86,45 +79,45 @@ const Login = () => {
                 <div className={styles.textWrapper}>
                     <div className={styles.titleContainer}>
                         <h1 className={styles.loginTitle}>Login</h1>
-                        <p className={styles.loginText}>
-                            Welcome back! Please enter your details.
-                        </p>
+                        <p className={styles.loginText}>Welcome back! Please enter your details.</p>
                     </div>
 
                     <form onSubmit={loginSubmit} className={styles.form}>
-                        {message &&                         
-                        <div className="flex items-center text-red-600 mb-3">
-                             <CgDanger size={25}/>
-                         <p className="ml-2 text-base">{message}</p>
-                        </div>}
+                        {message && (
+                            <div className="flex items-center text-red-600 mb-3">
+                                <CgDanger size={25} />
+                                <p className="ml-2 text-base">{message}</p>
+                            </div>
+                        )}
 
-                        <label className={styles.labelInput}>Email <br/>
-                            <input 
-                                type="text" 
+                        <label className={styles.labelInput}>
+                            Email <br />
+                            <input
+                                type="text"
                                 placeholder="user@gmail.com"
                                 className={styles.inputBox}
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                aria-label="Email"
                             />
                         </label>
-                      
-                        <label className={styles.labelInput}>Password  <br/>
-                            <input 
-                                type="password" 
+
+                        <label className={styles.labelInput}>
+                            Password <br />
+                            <input
+                                type="password"
                                 placeholder="Password"
                                 className={styles.inputBox}
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                                />
+                                aria-label="Password"
+                            />
                         </label>
 
                         <div className={styles.passwordContainer}>
                             <label className={styles.rememberText}>
-                                <input 
-                                    type="checkbox"
-                                    className={styles.checkboxInput}
-                                    />                              
-                                Remember for 30 days                      
+                                <input type="checkbox" className={styles.checkboxInput} />
+                                Remember for 30 days
                             </label>
 
                             <div className={styles.forgetPassword}>
@@ -132,23 +125,19 @@ const Login = () => {
                             </div>
                         </div>
 
-                        <button
-                           type="submit" 
-                          className={styles.signinBtn}>Sign in</button>
+                        <button type="submit" className={styles.signinBtn}>Sign in</button>
                     </form>
 
                     <div className={styles.signUpAcc}>
                         <p>Don&apos;t have an account? 
-                        <Link href="/signup">
-                            <span className={styles.signUp}>Sign up</span>
-                        </Link>
+                            <Link href="/signup">
+                                <span className={styles.signUp}>Sign up</span>
+                            </Link>
                         </p>
                     </div>
                 </div>
-                
             </div>
         </div>
-    //   </RegistrationProvider>
     );
 };
 
